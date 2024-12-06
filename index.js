c299da21353cc6825e55584fa258c217ff14c5e1
bill41 <willieboy14@outlook.com>
2024-12-06 16:49:31 -0500

    Create image carousel with clickable thumbnail images and slide transitions

--- index.js
+++ index.js
@@ -2,9 +2,10 @@
 const IMG_ID_DELIMITER = '-thumb';
 
 // carousel parms
-const OPACITY_DELTA = 0.01;
-const TIMER_DELAY = 15;
-const TRANSITION_DURATION_SECONDS = TIMER_DELAY / OPACITY_DELTA / 1000;
+const IMG_WIDTH = 260;
+const SLIDE_DELTA = 2;
+const TIMER_DELAY = 7;
+const TRANSITION_DURATION_SECONDS = (IMG_WIDTH / SLIDE_DELTA) * TIMER_DELAY / 1000;
 
 // all the main images
 const imgElements = document.querySelectorAll('div#carousel img');
@@ -13,9 +14,13 @@ const numImages = imgElements.length;
 // all the thumbnail images
 const thumbnailElements = document.querySelectorAll('div#thumbnails img');
 
-// show the first main image
+// position main images
+for (let i = 0; i < numImages; i++) {
+    imgElements[i].style.left = IMG_WIDTH * i + 'px';
+}
+
+// the first main image
 let imgCurrent = document.querySelector('div#carousel img:first-child');
-imgCurrent.style.opacity = 1;
 
 // click event listener for thumbnail images
 for (let thumbnailElement of thumbnailElements) {
@@ -41,35 +46,44 @@ for (let thumbnailElement of thumbnailElements) {
                 thumbnail.style.pointerEvents = 'none';
             }
 
-            // initialize opacities of current and next images
-            let opacityCurrent = 1;
-            let opacityNext = 0;
-            imgCurrent.style.opacity = opacityCurrent;
-            imgNext.style.opacity = opacityNext;
+            // initialize positions of current and next images
+            let leftCurrent = 0;
+            let leftNext;
+            if (Number(imgNext.dataset.order) > Number(imgCurrent.dataset.order)) {
+                leftNext = IMG_WIDTH;
+            } else {
+                leftNext = -IMG_WIDTH;
+            }
+            imgCurrent.style.left = leftCurrent + 'px';
+            imgNext.style.left = leftNext + 'px';
 
-            // fade out current image, fade in next image
+            // slide out current image, slide in next image
             const timerId = setInterval(() => {
 
                 // do the transition
-                opacityCurrent -= OPACITY_DELTA;
-                opacityNext += OPACITY_DELTA;
-                imgCurrent.style.opacity = opacityCurrent;
-                imgNext.style.opacity = opacityNext;
+                if (Number(imgNext.dataset.order) > Number(imgCurrent.dataset.order)) {
+                    leftCurrent -= SLIDE_DELTA;
+                    leftNext -= SLIDE_DELTA;
+                } else {
+                    leftCurrent += SLIDE_DELTA;
+                    leftNext += SLIDE_DELTA;
+                }
+                imgCurrent.style.left = leftCurrent + 'px';
+                imgNext.style.left = leftNext + 'px';
 
-                // fading complete
-                if (imgNext.style.opacity >= 1) {
+                // sliding complete
+                if (imgNext.style.left == 0 + 'px') {
 
-                    // set final opacities
+                    // cancel timer and update new current image
                     clearInterval(timerId);
-                    imgCurrent.style.opacity = 0;
-                    imgNext.style.opacity = 1;
                     imgCurrent = imgNext;
+                    imgCurrent.dataset.order = imgNext.dataset.order
 
                     // re-enable clicks on all thumbnails
                     for (let thumbnail of thumbnailElements) {
                         thumbnail.style.pointerEvents = 'auto';
                     }
-                }   // fading complete
+                }   // sliding complete
 
             }, TIMER_DELAY);
         }       // user clicks a different thumbnail
