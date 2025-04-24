dbaf25bcfd24c1b7b198bca01677430a42afdae9
bill41 <willieboy14@outlook.com>
2025-04-24 14:21:22 -0400

    Remove clickable thumbnail images, make slide transitions automatic

--- index.js
+++ index.js
@@ -1,91 +1,54 @@
-// image id delimiter
-const IMG_ID_DELIMITER = '-thumb';
-
 // carousel parms
 const IMG_WIDTH = 260;
 const SLIDE_DELTA = 2;
-const TIMER_DELAY = 7;
-const TRANSITION_DURATION_SECONDS = (IMG_WIDTH / SLIDE_DELTA) * TIMER_DELAY / 1000;
+const TIMER_DELAY_SLIDE = 7;
+const TIMER_DELAY_NEXT_IMG = 5000;
 
-// all the main images
+// all the images
 const imgElements = document.querySelectorAll('div#carousel img');
 const numImages = imgElements.length;
 
-// all the thumbnail images
-const thumbnailElements = document.querySelectorAll('div#thumbnails img');
-
-// position main images
+// position the images
 for (let i = 0; i < numImages; i++) {
     imgElements[i].style.left = IMG_WIDTH * i + 'px';
 }
 
-// the first main image
-let imgCurrent = document.querySelector('div#carousel img:first-child');
+// the first image
+let imgFirst = document.querySelector('div#carousel img:first-child');
+let imgCurrent = imgFirst;
 
-// click event listener for thumbnail images
-for (let thumbnailElement of thumbnailElements) {
+// transition to next image
+const timerIdNextImg = setInterval(() => {
 
-    thumbnailElement.addEventListener('click', evt => {
+    // get next image
+    let imgNext = imgCurrent.nextElementSibling;
+    if (!imgNext) {
+        imgNext = imgFirst;
+    }
 
-        // relationship of ids of thumbnails and main images
-        // id of thumbnail is "<img-name>-thumb", of main image is "<img-name>"
+    // initialize positions of current and next images
+    let leftCurrent = 0;
+    let leftNext;
+    leftNext = IMG_WIDTH;
+    imgCurrent.style.left = leftCurrent + 'px';
+    imgNext.style.left = leftNext + 'px';
 
-        // clicked thumbnail image
-        const thumbClicked = evt.currentTarget;
-        const thumbIdClicked = thumbClicked.getAttribute('id');
+    // slide out current image, slide in next image
+    const timerIdSlide = setInterval(() => {
 
-        // corresponding main image
-        const imgId = thumbIdClicked.split(IMG_ID_DELIMITER)[0];
-        const imgNext = document.getElementById(imgId);
+        // do the transition
+        leftCurrent -= SLIDE_DELTA;
+        leftNext -= SLIDE_DELTA;
+        imgCurrent.style.left = leftCurrent + 'px';
+        imgNext.style.left = leftNext + 'px';
 
-        // main image of clicked thumbnail different from current main image
-        if (imgNext != imgCurrent) {
+        // sliding complete
+        if (imgNext.style.left == 0 + 'px') {
 
-            // disable clicks on all thumbnails
-            for (let thumbnail of thumbnailElements) {
-                thumbnail.style.pointerEvents = 'none';
-            }
+            // cancel timer and update new current image
+            clearInterval(timerIdSlide);
+            imgCurrent = imgNext;
+        }   // sliding complete
 
-            // initialize positions of current and next images
-            let leftCurrent = 0;
-            let leftNext;
-            if (Number(imgNext.dataset.order) > Number(imgCurrent.dataset.order)) {
-                leftNext = IMG_WIDTH;
-            } else {
-                leftNext = -IMG_WIDTH;
-            }
-            imgCurrent.style.left = leftCurrent + 'px';
-            imgNext.style.left = leftNext + 'px';
-
-            // slide out current image, slide in next image
-            const timerId = setInterval(() => {
-
-                // do the transition
-                if (Number(imgNext.dataset.order) > Number(imgCurrent.dataset.order)) {
-                    leftCurrent -= SLIDE_DELTA;
-                    leftNext -= SLIDE_DELTA;
-                } else {
-                    leftCurrent += SLIDE_DELTA;
-                    leftNext += SLIDE_DELTA;
-                }
-                imgCurrent.style.left = leftCurrent + 'px';
-                imgNext.style.left = leftNext + 'px';
-
-                // sliding complete
-                if (imgNext.style.left == 0 + 'px') {
-
-                    // cancel timer and update new current image
-                    clearInterval(timerId);
-                    imgCurrent = imgNext;
-                    imgCurrent.dataset.order = imgNext.dataset.order
-
-                    // re-enable clicks on all thumbnails
-                    for (let thumbnail of thumbnailElements) {
-                        thumbnail.style.pointerEvents = 'auto';
-                    }
-                }   // sliding complete
-
-            }, TIMER_DELAY);
-        }       // user clicks a different thumbnail
-    });     // click event listener for thumbnails
-}   // for each thumbnail
+    }, TIMER_DELAY_SLIDE);    // slide timer
+}, TIMER_DELAY_NEXT_IMG);     // next image timer
