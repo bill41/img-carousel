10f18fb59f553222005885e46ceaffd4fd77daae
bill41 <willieboy14@outlook.com>
2025-04-29 11:27:25 -0400

    Get width of images in JS script for slide transitions from CSS variable

--- index.js
+++ index.js
@@ -1,51 +1,52 @@
 // carousel parms
-const OPACITY_DELTA = 0.01;
-const TIMER_DELAY_FADE = 15;
+const IMG_WIDTH = parseInt(
+  getComputedStyle(document.documentElement).getPropertyValue('--width-img')
+);
+const SLIDE_DELTA = 2;
+const TIMER_DELAY_SLIDE = 7;
 const TIMER_DELAY_NEXT_IMG = 5000;
 
 // all the images
 const imgElements = document.querySelectorAll('div#carousel img');
 const numImages = imgElements.length;
 
-// show the first image
+// position the images
+for (let i = 0; i < numImages; i++) {
+  imgElements[i].style.left = IMG_WIDTH * i + 'px';
+}
+
+// the first image
 let imgFirst = document.querySelector('div#carousel img:first-child');
-imgFirst.style.opacity = 1;
 let imgCurrent = imgFirst;
 
 // transition to next image
 const timerIdNextImg = setInterval(() => {
+  // get next image
+  let imgNext = imgCurrent.nextElementSibling;
+  if (!imgNext) {
+    imgNext = imgFirst;
+  }
 
-    // get next image
-    let imgNext = imgCurrent.nextElementSibling;
-    if (!imgNext) {
-        imgNext = imgFirst;
-    }
+  // initialize positions of current and next images
+  let leftCurrent = 0;
+  let leftNext;
+  leftNext = IMG_WIDTH;
+  imgCurrent.style.left = leftCurrent + 'px';
+  imgNext.style.left = leftNext + 'px';
 
-    // initialize opacities of current and next images
-    let opacityCurrent = 1;
-    let opacityNext = 0;
-    imgCurrent.style.opacity = opacityCurrent;
-    imgNext.style.opacity = opacityNext;
+  // slide out current image, slide in next image
+  const timerIdSlide = setInterval(() => {
+    // do the transition
+    leftCurrent -= SLIDE_DELTA;
+    leftNext -= SLIDE_DELTA;
+    imgCurrent.style.left = leftCurrent + 'px';
+    imgNext.style.left = leftNext + 'px';
 
-    // fade out current image, fade in next image
-    const timerIdFade = setInterval(() => {
-
-        // do the transition
-        opacityCurrent -= OPACITY_DELTA;
-        opacityNext += OPACITY_DELTA;
-        imgCurrent.style.opacity = opacityCurrent;
-        imgNext.style.opacity = opacityNext;
-
-        // fading complete
-        if (imgNext.style.opacity >= 1) {
-
-            // set final opacities
-            clearInterval(timerIdFade);
-            imgCurrent.style.opacity = 0;
-            imgNext.style.opacity = 1;
-            imgCurrent = imgNext;
-
-        }   // fading complete
-
-    }, TIMER_DELAY_FADE);    // fade timer
-}, TIMER_DELAY_NEXT_IMG);     // next image timer
+    // sliding complete
+    if (imgNext.style.left == 0 + 'px') {
+      // cancel timer and update new current image
+      clearInterval(timerIdSlide);
+      imgCurrent = imgNext;
+    } // sliding complete
+  }, TIMER_DELAY_SLIDE); // slide timer
+}, TIMER_DELAY_NEXT_IMG); // next image timer
